54c9147ec699a9261a46a9169d61134e4b337556
sebasphere <sebasphereemails@gmail.com>
2026-01-18 05:31:43 -0500

    feat: fabric working again

--- common/src/main/java/dev/sebastianb/owocraft/mixin/client/YACLScreenMixin.java
+++ common/src/main/java/dev/sebastianb/owocraft/mixin/client/YACLScreenMixin.java
@@ -16,23 +16,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(YACLScreen.class)
-public abstract class TitleScreenMixin {
+public abstract class YACLScreenMixin {
 
     @Unique
     HapticsIconWidget hapticsIconWidget = new HapticsIconWidget(0, 32, 32, 32);
 
-    @Shadow
+    @Shadow(remap = false)
     @Final
     public YetAnotherConfigLib config;
 
-    @Shadow @Final private Screen parent;
+    @Shadow(remap = false) @Final private Screen parent;
 
-    @Inject(at = @At("TAIL"), method = "init")
+    @Inject(at = @At("TAIL"), method = "init", remap = false)
     public void thing(CallbackInfo ci) {
 
     }
 
-    @Inject(at = @At("TAIL"), method = "renderBackground")
+    @Inject(at = @At("TAIL"), method = "renderBackground", remap = false)
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         YACLScreen s = ((YACLScreen) (Object) this);
         if (this.config.title().equals(Component.literal("Owocraft Config"))
